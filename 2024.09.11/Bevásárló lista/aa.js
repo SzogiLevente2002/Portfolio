196e738a29f0e56674e7fcb21f290d5a18d449bc
SzogiLevente2002 <144908048+SzogiLevente2002@users.noreply.github.com>
2024-11-05 20:08:28 +0100

    Update aa.js

--- 2024.09.11/Bevásárló lista/aa.js
+++ 2024.09.11/Bevásárló lista/aa.js
@@ -1,46 +1,30 @@
-var b1 = []
+var bl =[]
 function refresh() {
     document
-    .querySelector('#output')
-    .innerHTML = 
-    b1.map((elem,i) => {
-        [t,p] = elem
-        return `
-            <div class = "x${i % 2} y${p}">7
-            ${t}
-            <span onclick = "g('${i}')">/<span>
-            <span onclick = "del('${t}')">/<span>
-            </div>
-            `
-
-
-    }).join('')
+        .querySelector('#output')
+        .innerHTML = 
+        bl.map((elem, i) => {
+            [t, p] = elem
+            return `
+                <div  class="x${i % 2}">
+                    <span class="y y${p}">${t}</span>
+                    <span class="g g1" onclick="g('${i}')">✔</span>
+                    <span class="g g2"onclick="del('${t}')">✘</span>
+                </div>
+             `
+        }).join('')
 }
 function f(e) {
-    b1.push([e.value,0])
-    b1.sort()
-    e.value = ''
+    bl.push([e.value, 0])
+    bl.sort()
+    e.value=''
     refresh()
 }
 function g(i) {
-    b1[i][1] = b1[i][1] == 0 ? 1 : 0
+    bl[i][1] = bl[i][1] == 0 ? 1 : 0
     refresh()
 }
 function del(e) {
-    b1 = b1.filter(v => v[0] != e)
+    bl = bl.filter(v => v[0] != e)
     refresh()
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+} 
