f514131539bf9396f99d4924ad25e57655968fe7
SzogiLevente2002 <144908048+SzogiLevente2002@users.noreply.github.com>
2024-12-04 09:51:12 +0100

    Delete 2024.09.19/promise directory

--- 2024.09.19/promise/01.js
+++
@@ -1,6 +0,0 @@
-var i = 1;
-setInterval(a => {
-    i*= a,
-    console.log(i);
-},1.2,5);
-console.log(i);
--- 2024.09.19/promise/02.js
+++
@@ -1,8 +0,0 @@
-x = new Promise((a,b)=>{
-    if (Math.random() > .1) {
-        a(Math.round(Math.random()*100))
-    } else {
-        b("Hiba")
-    }
-})
-x.then(console.log).catch(console.error)
