576a526029e77006227878b43406e56be5fb5441
SzogiLevente2002 <144908048+SzogiLevente2002@users.noreply.github.com>
2024-12-04 09:50:19 +0100

    Delete 2024.09.05.Elso_Feladat directory

--- 2024.09.05.Elso_Feladat/valami.js
+++
@@ -1,6 +0,0 @@
-const évszakok = ['ősz','tél','tavasz','nyár']
-function init() {
-    document.getElementById('body').innerHTML = evszakok.map(v=>
-        `div onclick="f('${v}')">${v}</div>`
-    ).join("")
-}
